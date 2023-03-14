8b347efbafbd03d11055ffe26ba876c55904d5e2
Irina Jinar <irinajinar.simedru@gmail.com>
2023-03-14 21:59:23 +0200

    Added initial project

---
+++ src/test/java/com/project/CakeShop/CakeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CakeShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
