8a8b8c69e2d57d4cfc29be5deb119873bd27560c
Irina Jinar <irinajinar.simedru@gmail.com>
2023-03-16 11:25:55 +0200

    Added: register and login endpoints, and user and product entities.

--- src/main/java/com/project/CakeShop/service/AuthService.java
+++ src/main/java/com/project/CakeShop/service/AuthService.java
@@ -5,9 +5,7 @@ import com.project.CakeShop.model.dto.UserLoginDto;
 import com.project.CakeShop.model.dto.UserRegisterDto;
 
 public interface AuthService {
-    //metoda register
     User register(UserRegisterDto userRegisterDto) throws Exception;
-    //metoda login
 
     User login(UserLoginDto userLoginDto) throws Exception;
 }
